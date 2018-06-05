007e28733c1dd79c196cdb98d1915426bba85dc9
Matt Erickson <Matt@MattErickson.ME>
2018-06-05 10:55:27 -0500

    Updating auto-pull-req-new-mnufc.js

--- auto-pull-req-new-mnufc.js
+++ auto-pull-req-new-mnufc.js
@@ -3,8 +3,10 @@ module.exports = function(cb) {
   
     var query = new YQL("select * from fantasysports.games where game_key='238'");
     
-    query.exec(function(err, data) {
-      console.log(data);
+    var data;
+    require('request').query.exec(function(err, theDat) {
+      data = theDat;
+      console.log(theDat);
     });
   cb(null, { msg: data});
 };
