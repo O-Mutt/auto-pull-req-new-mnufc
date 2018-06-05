13f34ee6958dd901b8784d01c123b53c7992a06d
Matt Erickson <Matt@MattErickson.ME>
2018-06-05 10:56:34 -0500

    Updating auto-pull-req-new-mnufc.js

--- auto-pull-req-new-mnufc.js
+++ auto-pull-req-new-mnufc.js
@@ -1,7 +1,7 @@
 var YQL = require('yql');
 module.exports = function(cb) {
   
-    var query = new YQL("select * from fantasysports.games where game_key='238'");
+    var query = new YQL("SELECT * FROM weather.forecast WHERE (location = 94089)");
     
     var data;
     query.exec(function(err, theDat) {
