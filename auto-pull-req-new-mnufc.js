bea78451bfc032ae758cf728914862f000eb12a8
Matt Erickson <Matt@MattErickson.ME>
2018-06-07 20:33:22 -0500

    Updating auto-pull-req-new-mnufc.js

--- auto-pull-req-new-mnufc.js
+++ auto-pull-req-new-mnufc.js
@@ -1,3 +1,5 @@
+
+
 'use latest';
 
 const request = require('request');
@@ -6,32 +8,50 @@ const _ = require('lodash');
 const cheerio = require('cheerio');
 const Promises = require('bluebird');
 const hostname = 'https://www.mnufc.com'
-let iframeUrlTemplate = `<div class='fluid-width-video-wrapper'><iframe width='100%' height='auto' frameborder='0' allowfullscreen src='https://www.mnufc.com/iframe-video?brightcove_id=`;
-let iframUrlTemplatePt2 = `&brightcove_player_id=default&brightcove_account_id=5534894110001'></iframe></div>`;
+
+let postHeader = ` 
+author: Matt Erickson (ME)
+layout: post
+categories:
+  - MNUFC
+  - Auto-post
+tags:
+  - MNUFC
+  - Soccer
+---`
+
+
+let iframeUrlTemplate = `<div class='fluid-width-video-wrapper'><iframe width='100%' height='auto' frameborder='0' allowfullscreen src="https://www.mnufc.com/iframe-video?brightcove_id={replaceMe}&brightcove_player_id=default&brightcove_account_id=5534894110001"></iframe></div>`;
 
 /**
  * @param context {WebtaskContext}
  */
 module.exports = function(context, cb) {
-
-    var highlightArray = [];
+    let highlightArray = [];
+    let videoPromises = [];
     rp({
         uri: hostname + '/video-channels/match-highlights',
         transform: function(body) {
             return cheerio.load(body);
         }
-    }).then(function($) { 
+      }).then(function($) { 
         //data process 
-        var videoPromises = [];
-        _.forEach($(".views-row .node"), function(node) {
-          console.log("fffuuuu");
-            let highlight = $.find('.node-title a');
+        _.map($(".views-row .node"), function(node) {
+            let highlight = $(node).find('.node-title a');
+            
             let title = highlight.text();
-            console.log(title);
+            let titleWithoutEndDate = title.replace(/\|.*/gi, '').replace('\.', '');
+            
             let postUrl = highlight.attr('href');
-            var date = $.find('.timestamp').text();
-            highlight = $(highlight);
-            highlightArray.push({ title: title, postUrl: postUrl, date: date });
+            var date =  new Date($(node).find('.timestamp').text().replace(/\s\(.*\)/gi, ''));
+            let filename = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + '-' + titleWithoutEndDate;
+            highlightArray.push({ 
+              filename: filename,
+              title: title, 
+              postUrl: postUrl, 
+              date: date,
+              permalink: permalink
+            });
         });
 
         _.forEach(highlightArray, function(highlight) {
@@ -43,16 +63,59 @@ module.exports = function(context, cb) {
             });
             videoPromises.push(vidProm);
         });
+        videoPromises.push
 
         Promises.all(videoPromises).then(function(videos) {
-            for (var i = 0; i < highlightArray.length - 1; i++) {
-                highlightArray[i].video = iframeUrlTemplate + videos[i]('video').attr('data-video-id') + iframUrlTemplatePt2;
+            for (var i = 0; i < highlightArray.length; i++) {
+              videoHtml = iframeUrlTemplate.replace('{replaceMe}', videos[i]('video').attr('data-video-id'));
+              highlightArray[i].video = videoHtml;
             }
-
-            //outputHighlightPostFile(highlightArray[0])
-            cb(null, { highlights: highlightArray });
+            mapHighlightsToGitHub(highlightArray);
         });
     });
 
-    //cb(null, { theDom });
+  function mapHighlightsToGitHub(allHighlights) {
+    rp({
+      qs: {
+        access_token: context.secrets.GITHUB_ACCESS_TOKEN
+      },
+      headers: {
+        'User-Agent': 'MN UFC auto blogger'
+      },
+      json: true,
+      uri: `https://api.github.com/repos/Mutmatt/mutmatt.github.io/commits?path=_posts/mnufc/`
+    }).then(function(response) {
+      let newPosts = _.differenceWith(allHighlights, response, function(mnufcValue, githubObject) {
+        return mnufcValue.title == githubObject.commit.message;
+      });
+      
+      _.forEach(newPosts, function(post) {
+        console.log('create new file to gh ' + post.title);
+
+        var postText = `---
+title: ${post.title},
+date: ${post.date},
+permalink: /${_.snakeCase(post.title)}` + postHeader + '\r\n' + post.video;
+
+        rp.put({
+          qs: {
+            access_token: context.secrets.GITHUB_ACCESS_TOKEN
+          },
+          headers: {
+            'User-Agent': 'MN UFC auto blogger'
+          },
+          json: true,
+          method: 'PUT',
+          uri: `https://api.github.com/repos/Mutmatt/mutmatt.github.io/contents/_posts/mnufc/` + _.camelCase(post.title) + '.md',
+          body: {
+            path: '_posts/mnufc',
+            message: post.title,
+            content: Buffer.from(postText).toString('base64')
+          }
+        });
+      });
+      
+      cb(null, { newPosts });
+    });
+  }
 };
